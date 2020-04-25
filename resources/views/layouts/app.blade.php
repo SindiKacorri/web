32cdc1cf16c35b150eb6283d19fdb39256c5cbf0
Terri Prifti <terriprifti@gmail.com>
2020-04-25 16:23:12 +0200

    vue removed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Projekt Informatike</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 </head>
 <body>
@@ -30,7 +26,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        Projekt Informatike
                     </a>
                 </div>
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,4 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
-
-   .combine(['resources/assets/css/custom.css'], 'public/css/custom.css').version();
+   .sass('resources/assets/css/custom.scss', 'public/css').version();
